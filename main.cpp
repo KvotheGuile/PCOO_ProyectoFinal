7d649eaecdcc6d0fb2afe6ae14620d3ae55c9350
KvotheGuile <kvothejoestar@gmail.com>
2024-11-22 10:11:18 -0700

    First commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include "School.h"
+#include "Administrator.h"
+#include "Professor.h"
+#include "Student.h"
+
+int main()
+{
+    std::cout<<"Hello 0/100"<<std::endl;
+
+    return 0;
+}
