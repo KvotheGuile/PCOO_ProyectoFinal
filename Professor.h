08accc7e3cb9a8c050335233635ea68d65aa295c
KvotheGuile <kvothejoestar@gmail.com>
2024-11-22 16:43:16 -0700

    Professor.h completado

--- Professor.h
+++ Professor.h
@@ -2,9 +2,29 @@
 #ifndef PROFESSOR_H
 #define PROFESSOR_H
 
+#include <iostream> 
+#include <string>
+using namespace std;
+
 class Professor
 {
+    private:
+    string nombre;
+    string matricula;
+    double salario;
+    double saldo;
 
+    public:
+    Professor();
+    Professor(string _nombre, string _matricula, double _salario);
+    ~Professor();
+
+    double getSalario();
+    void setSalario(double);
+    void aumentarSalario(double);
+    double pagar(double);
+
+    void printInfo();
 };
 
 #endif
