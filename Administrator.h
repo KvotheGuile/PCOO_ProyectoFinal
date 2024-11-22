a5b0611cf752dd164122c56c32258d11e91b320e
KvotheGuile <kvothejoestar@gmail.com>
2024-11-22 16:48:44 -0700

    Administrador.h completado

--- Administrator.h
+++ Administrator.h
@@ -2,8 +2,27 @@
 #ifndef ADMINISTRATOR_H
 #define ADMINISTRATOR_H
 
+#include <iostream> 
+#include <string>
+using namespace std;
+
+
 class Administrador
 {
+    private:
+    string usuario;
+    string password;
+    bool loggedIn;
+
+    public:
+    Administrador(string _usuario, string _password);
+    ~Administrador();
+
+    bool esUsuario(string _usuario);
+    void cambiarPassword(string _password);
+    bool logIn(string _passwordIntento); // Regresa si fue existoso el intento
+    void logOff();
+    bool on(); // Regresa loggedIn
 
 };
 
