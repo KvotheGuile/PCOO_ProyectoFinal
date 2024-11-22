389827ff6fecb6db04c4c7bc4549bb728446f0e8
KvotheGuile <kvothejoestar@gmail.com>
2024-11-22 16:34:23 -0700

    Student.h completado

--- Student.h
+++ Student.h
@@ -2,9 +2,32 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+#include <iostream> 
+#include <string>
+using namespace std;
+
 class Student
 {
+    private:
+    string nombre;
+    string matricula;
+    double estadoDeCuenta;
+    float beca;
 
+    public:
+    Student();
+    Student(string _nombre, string _matricula, float _beca);
+
+    void setBeca(float _becaNueva);
+    float getBeca();
+    
+    bool esEstudiante(string _matricula);
+    double recolectarDinero(double _colegiatura); 
+    /* ^^^ Ingresas la colegiatura, aplica la beca.
+    Esta cantidad se le resta al estado de cuenta y luego se regresa
+    */
+
+    void printInfo();
 };
 
 #endif
